eb39b4bfe27f8ce6cabc03e1109e003807771db0
William McPherson <willmcpherson2@gmail.com>
2023-09-30 14:22:07 +1000

    Utilise unused keys

--- keyboards/crkbd/keymaps/willkbd/keymap.c
+++ keyboards/crkbd/keymaps/willkbd/keymap.c
@@ -14,9 +14,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [1] = LAYOUT_split_3x6_3(
     //|---------|---------|---------|---------|---------|---------|    |---------|---------|---------|---------|---------|---------|
-        KC_ESC  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   ,      KC_NO   , KC_MPRV , KC_VOLD , KC_VOLU , KC_MNXT , KC_BSPC ,
+        KC_ESC  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   ,      KC_MPLY , KC_MPRV , KC_VOLD , KC_VOLU , KC_MNXT , KC_BSPC ,
     //|---------|---------|---------|---------|---------|---------|    |---------|---------|---------|---------|---------|---------|
-        KC_TAB  , KC_GRV  , KC_MINS , KC_EQL  , KC_LBRC , KC_RBRC ,      KC_BSLS , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , KC_NO   ,
+        KC_TAB  , KC_GRV  , KC_MINS , KC_EQL  , KC_LBRC , KC_RBRC ,      KC_BSLS , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , KC_QUOT ,
     //|---------|---------|---------|---------|---------|---------|    |---------|---------|---------|---------|---------|---------|
         KC_RCTL , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,      KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_ENT  ,
     //|---------|---------|---------|---------|---------|---------|    |---------|---------|---------|---------|---------|---------|
